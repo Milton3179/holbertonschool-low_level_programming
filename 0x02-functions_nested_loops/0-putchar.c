d7a2a672cffc0df7259ce94fcd28bd0ed0272b02
Milton3179 <3939holberton.com>
2021-10-07 18:13:27 -0500

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+#include "main.h"
+
+/**
+ *main - print putchar
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	char c[] = "_putchar";
+	int i;
+
+	for (i = 0; i <= 7; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
+
+
+
+
+
+
+
